a55f900a05c831e3be909c416d74b7b8f3bad94b
Jakub Dzik <abukaj@users.noreply.github.com>
2020-06-17 00:56:58 +0200

    Fix #3
    Commandline argument `--phase-space` added.

--- main.py
+++ main.py
@@ -49,6 +49,48 @@ def draw(df: pandas.DataFrame):
     plt.show()
 
 
+def draw_phase_space(df: pandas.DataFrame):
+    # These are the colors that will be used in the plot
+    color_sequence = ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c',
+                      '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5',
+                      '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f',
+                      '#c7c7c7']
+
+    fig, ax = plt.subplots(1, 1, figsize=(12, 14))
+
+    ax.spines['top'].set_visible(True)
+    ax.spines['bottom'].set_visible(True)
+    ax.spines['right'].set_visible(True)
+    ax.spines['left'].set_visible(True)
+
+    # ax.get_xaxis().tick_bottom()
+    # ax.get_yaxis().tick_left()
+
+    fig.subplots_adjust(left=.06, right=.94, bottom=.07, top=.94)
+
+    ax.xaxis.set_major_formatter(plt.FuncFormatter('{:.3g}'.format))
+    ax.yaxis.set_major_formatter(plt.FuncFormatter('{:.3g}'.format))
+
+    # plt.grid(True, 'major', 'y', ls='--', lw=.5, c='k', alpha=.3)
+
+    plt.tick_params(axis='both', which='both', bottom='on', top='off',
+                    labelbottom='on', left='on', right='off', labelleft='on')
+
+    plt.xlabel("total cases")
+    plt.ylabel("new cases")
+    # plt.xticks(x_range, df.date.tolist(), rotation=90, fontsize=10)
+    # plt.yticks(np.arange(start=0, step=50, stop=500), fontsize=10)
+    for column, color in zip(df.columns[1:], color_sequence):
+        new_cases = df[column].tolist()
+        line = plt.plot(np.cumsum(new_cases), new_cases, lw=2.5,
+                        color=color, label=column)
+        mplcursors.cursor(line)
+    fig.suptitle('COVID-19 in Poland by region', fontsize=18, ha='center')
+
+    plt.legend()
+    plt.show()
+
+
 POPULATION = {
               'dolnośląskie': 2_901_225,
               'kujawsko-pomorskie': 2_077_775,
@@ -86,8 +128,11 @@ if __name__ == '__main__':
     parser.add_argument('--normalize', dest='normalize', action='store_const',
                         const=normalize, default=lambda x: x,
                         help='normalize data by population size [100 000]')
+    parser.add_argument('--phase-space', dest='draw', action='store_const',
+                        const=draw_phase_space, default=draw,
+                        help='plot data in phase-space')
     args = parser.parse_args()
 
     covid = CovidStats()
     df = covid.get_data()
-    draw(args.normalize(df))
+    args.draw(args.normalize(df))
